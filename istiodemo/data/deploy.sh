50afaa7af6eb8e7dcc57e66ef3165a8d036a4832
Johannes Engelke <johannes.engelke@sap.com>
2018-05-14 19:55:11 +0200

    added content

---
+++ istiodemo/data/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+export ETCDCTL_API=3
+
+for var in `ls -1 | grep json`
+do 
+   echo $var
+   kubectl exec -c etcd -it $(kubectl get pods -lapp=etcd --no-headers=true -o custom-columns=:metadata.name) -- /usr/local/bin/etcdctl put $(echo $var | sed 's/.json//') "$(cat $var)"
+done
